b10dd6cc94b27aee101d73516b620ff4bddc7d6c
maddenlockin <maddenlockin@gmail.com>
2021-04-28 15:41:25 -0700

    style changes and start of name change button

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const myInput = document.getElementById('my-input');
+
+const myButton = document.getElementById('my-button');
+
+const wordBox = document.getElementById('word-box')
+
+myButton.addEventListener('click', () => {
+    const userInput = theInput.value;
+    wordBox.textContent = userInput.value;
+})
+
+
+
+
